0d028bb5697e37a80f815d3706f923c5cd53d84c
Nick Q. <nicky_qianlei@163.com>
2013-09-16 14:17:04 +0800

    set up Spring MVC environment.

--- src/nicky/controller/IndexController.java
+++ src/nicky/controller/IndexController.java
@@ -2,11 +2,19 @@ package nicky.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 @Controller
-public class IndexController {
-    @RequestMapping("/index")
-    public String indexView() {
-            return "TEST~~~";
+public class IndexController extends MultiActionController{
+    @RequestMapping("/")
+    public ModelAndView index() {
+        return indexView();
+    }
+    @RequestMapping(value = "/index")
+    public ModelAndView indexView() {
+        ModelAndView mav = new ModelAndView("index");
+//        mav.addObject("name", "Nicky!");
+        return mav;
     }
 }
